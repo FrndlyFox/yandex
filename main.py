a44c5b80b0ade6cf354ef200b5d26ee7af658512
FrndlyFox <frndlyfox@mail.ru>
2021-12-01 17:45:46 +0300

    много кружков и разного цвета теперь да

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import sys
 from random import randint as rand
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ui import Ui_MainWindow
 
-class Main(QtWidgets.QMainWindow):
+class Main(QtWidgets.QMainWindow, Ui_MainWindow):
 	def __init__(self):
 		super().__init__()
 		self.clicked = False
-		self.setupUi()
-
-	def setupUi(self):
-		uic.loadUi('UI.ui', self)
+		self.setupUi(self)
 		self.btn.clicked.connect(self.click)
 		self.sz = (self.size().width(), self.size().height())
 
 	def click(self):
 		self.clicked = True
+		self.btn.hide()
 		self.update()
 
 	def paintEvent(self, event):
 		if self.clicked:
-			self.btn.hide()
 			qp = QtGui.QPainter()
 			qp.begin(self)
-			qp.setBrush(QtGui.QColor(255, 255, 0))
-			for i in range(rand(1, 5)):
+			for i in range(rand(1, 20)):
+				qp.setBrush(QtGui.QColor(*[rand(0, 255) for i in range(3)]))
 				rad = rand(5, 20)
 				pos = [rand(0, self.sz[0] - rad * 2), rand(0, self.sz[1] - rad * 2)]
 				qp.drawEllipse(pos[0], pos[1], rad * 2, rad * 2)
